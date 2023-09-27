a4809f7281ae4770a4f6bd21052017549319612e
aristote <aryaristote@gmail.com>
2023-09-27 10:57:51 +0200

    session management

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,18 +6,20 @@ import { useEffect, useState } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
+  const isTHeUserlogin = true;
   // const { data: session } = useSession();
 
-  // const [providers, setProviders] = useState(null);
-  // const [toggleDropdown, setToggleDropdown] = useState(false);
+  const [providers, setProviders] = useState(null);
+  const [toggleDropdown, setToggleDropdown] = useState(false);
 
-  // useEffect(() => {
-  //   (async () => {
-  //     const res = await getProviders();
-  //     setProviders(res);
-  //   })();
-  // }, []);
-  const isTHeUserlogin = true;
+  useEffect(() => {
+    (async () => {
+      const res = await getProviders();
+
+      setProviders(res);
+    })();
+  }, []);
+
 
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
@@ -45,43 +47,42 @@ const Nav = () => {
               Sign Out
             </button>
 
-            {/* <Link href='/profile'>
+            <Link href='/profile'>
               <Image
-                src={session?.user.image}
+                src="/assets/images/logo.svg"
                 width={37}
                 height={37}
                 className='rounded-full'
                 alt='profile'
               />
-            </Link> */}
+            </Link>
           </div>
-        ) : (
-          <></>
-        // ) : ( 
-        //    <>
-        //     {providers &&
-        //       Object.values(providers).map((provider) => (
-        //         <button
-        //           type='button'
-        //           key={provider.name}
-        //           onClick={() => {
-        //             signIn(provider.id);
-        //           }}
-        //           className='black_btn'
-        //         >
-        //           Sign in
-        //         </button>
-        //       ))}
-        //  </> 
+        ) : (  
+           <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type='button'
+                  key={provider.name}
+                  onClick={() => {
+                    signIn(provider.id);
+                  }}
+                  className='black_btn'
+                >
+                  Sign in
+                </button>
+              ))}
+         </> 
         )}
       </div> 
 
       {/* Mobile Navigation */}
-      {/* <div className='sm:hidden flex relative'>
-        {session?.user ? (
+      <div className='sm:hidden flex relative'>
+        {/* {session?.user ? ( */}
+        {isTHeUserlogin ? (
           <div className='flex'>
             <Image
-              src={session?.user.image}
+              src="/assets/images/logo.svg"
               width={37}
               height={37}
               className='rounded-full'
@@ -135,7 +136,7 @@ const Nav = () => {
               ))}
           </>
         )}
-      </div> */}
+      </div>
     </nav>
   );
 };
